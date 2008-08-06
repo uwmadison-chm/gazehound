dbcb98c7147150acac4d2742d4866a5a7afd53d8
njvack <njvack@7159bfc6-6853-0410-b48a-472db7bb305d>
2008-08-06 17:41:33 +0000

    Modified svn:ignore

--- setup.py
+++ setup.py
@@ -1,3 +1,7 @@
+import ez_setup
+ez_setup.use_setuptools()
+
+
 from setuptools import setup, find_packages
 setup(
     name = "gazehound",
@@ -10,10 +14,21 @@ setup(
     # metadata for upload to PyPI
     author = "Nathan Vack",
     author_email = "njvack@wisc.edu",
-    description = "Utilities for viewing and analyzing gaze tracking (and eyetracking) data",
+    description = ("Utilities for viewing and analyzing gaze tracking (and "+
+        "eyetracking) data"),
     license = "GPL 2.0",
-    keywords = "gazetracking eyetracking  world example examples",
-    url = "http://code.google.com/p/gazehound",   # project home page, if any
-
-    # could also include long_description, download_url, classifiers, etc.
+    keywords = "gazetracking eyetracking psychology science research",
+    url = "http://code.google.com/p/gazehound",
+    classifiers = (
+        "Development Status :: 1 - Planning",
+        "Environment :: Console",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Scientific/Engineering :: Information Analysis"
+    )
 )
