1cb11fe65c17f0a8dd69240bb3fc3939ee2d2938
njvack <njvack@7159bfc6-6853-0410-b48a-472db7bb305d>
2008-08-22 21:22:01 +0000

    Added license / copyright header

--- tests/presentation_test.py
+++ tests/presentation_test.py
@@ -1,3 +1,10 @@
+# Part of the gazehound package for analzying eyetracking data
+#
+# Copyright (c) 2008 Board of Regents of the University of Wisconsin System
+#
+# Written by Nathan Vack <njvack@wisc.edu> at the Waisman Laborotory
+# for Brain Imaging and Behavior, University of Wisconsin - Madison.
+
 import gazehound.stimulus
 from gazehound.stimulus import presentation
 
