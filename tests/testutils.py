982c50ef1061e878d384fbf615b5abe72e457723
njvack <njvack@7159bfc6-6853-0410-b48a-472db7bb305d>
2008-09-24 18:18:39 +0000

    Added some utilities that I can't believe aren't part of nose.tools

---
+++ tests/testutils.py
@@ -0,0 +1,28 @@
+# Part of the gazehound package for analzying eyetracking data
+#
+# Copyright (c) 2008 Board of Regents of the University of Wisconsin System
+#
+# Written by Nathan Vack <njvack@wisc.edu> at the Waisman Laborotory
+# for Brain Imaging and Behavior, University of Wisconsin - Madison.
+
+def gt_(a, b, msg = None):
+    """Shorthand for 'assert a > b, "%r != %r" % (a, b)
+    """
+    assert a > b, msg or "%r <= %r" % (a, b)
+
+def gte_(a, b, msg = None):
+    """Shorthand for 'assert a >= b, "%r != %r" % (a, b)
+    """
+    assert a >= b, msg or "%r < %r" % (a, b)
+
+def lt_(a, b, msg = None):
+    """Shorthand for 'assert a < b, "%r != %r" % (a, b)
+    """
+    assert a < b, msg or "%r >= %r" % (a, b)
+
+
+def lte_(a, b, msg = None):
+    """Shorthand for 'assert a <= b, "%r != %r" % (a, b)
+    """
+    assert a <= b, msg or "%r > %r" % (a, b)
+
