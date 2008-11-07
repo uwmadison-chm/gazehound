bb059f416db46ce905b2da39ae33950632967b56
njvack <njvack@7159bfc6-6853-0410-b48a-472db7bb305d>
2008-11-07 17:56:35 +0000

    Added view_plotter module, to handle producing images of gaze data.

---
+++ src/gazehound/view_plotter.py
@@ -0,0 +1,87 @@
+# Part of the gazehound package for analzying eyetracking data
+#
+# Copyright (c) 2008 Board of Regents of the University of Wisconsin System
+#
+# Written by Nathan Vack <njvack@wisc.edu> at the Waisman Laborotory
+# for Brain Imaging and Behavior, University of Wisconsin - Madison.
+
+# This module contains tools to convert scanpaths into images indicating
+# where a subject's gaze lingered.
+
+import array
+from ext import png
+
+class ScanpathPlotter(object):
+    """Plots scanpath on a canvas"""
+    def __init__(self, canvas, scanpath, view_matrix):
+        super(ScanpathPlotter, self).__init__()
+        self.canvas = canvas
+        self.scanpath = scanpath
+        self.view_matrix = view_matrix
+        
+    
+    def draw_scanpath(self):
+        for point in self.scanpath:
+            self.canvas.add_matrix(self.view_matrix, (point.x, point.y))
+        
+        
+
+class Canvas(object):
+    """docstring for Canvas"""
+    def __init__(self, width, height, type_str = 'f', fill_value = 0.0):
+        super(Canvas, self).__init__()
+        self.data = [
+            array.array(type_str, [fill_value]*height)
+            for e in range(0, width)
+        ]
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, i):
+        return self.data[i]
+
+    def width(self):
+        return len(self.data)
+    
+    def height(self):
+        return len(self.data[0])
+        
+    def add_matrix(self, source, target_point):
+        # Correct for target_point indicating center
+        px, py = (
+            (target_point[0]-len(source)/2), 
+            (target_point[1]-len(source[0])/2))
+        # Compute ranges for source and target...
+        sx1 = min(max(0, -px), len(source))
+        sy1 = min(max(0, -py), len(source[0]))
+        sx2 = max(min(len(source), (self.width() - px)), 0)
+        sy2 = max(min(len(source[0]), (self.height() - py)), 0)
+        
+        tx1 = min(max(0, px), self.width())
+        ty1 = min(max(0, py), self.height())
+        tx2 = max(min(self.width(), (len(source) + px)), 0)
+        ty2 = max(min(self.height(), (len(source[0]) + py)), 0)
+        
+        xcount = min((sx2-sx1), (tx2-tx1))
+        ycount = min((sy2-sy1), (ty2-ty1))
+        
+        for i in range(0, xcount):
+            for j in range(0, ycount):
+                self[tx1+i][ty1+j] += source[sx1+i][sy1+j]
+        
+    def to_trans_bytes(self):
+        #colorstr = '\xcc\xcc\xcc\xcc\xcc\xcc'
+        rows = []
+        for rcount in range(0, self.height()):
+            row = []
+            for col in range(0, self.width()):
+                row.extend([128,128,128,int(255*(1-self.data[col][rcount]))])
+            rows.append(array.array('B', row))
+        return rows
+
+    def to_trans_file(self, filename):
+        f = open(filename, 'w')
+        w = png.Writer(self.width(),self.height(), has_alpha=True)
+        w.write(f, self.to_trans_bytes())
+        f.close()
---
+++ tests/view_plotter_test.py
@@ -0,0 +1,110 @@
+# Part of the gazehound package for analzying eyetracking data
+#
+# Copyright (c) 2008 Board of Regents of the University of Wisconsin System
+#
+# Written by Nathan Vack <njvack@wisc.edu> at the Waisman Laborotory
+# for Brain Imaging and Behavior, University of Wisconsin - Madison.
+
+from nose.tools import eq_
+from testutils import gt_, lt_, gte_, lte_, includes_
+import mock_objects
+
+import array
+
+from gazehound import view_plotter, shapes
+
+class TestScanpathPlotter(object):
+    def __init__(self):
+        super(TestScanpathPlotter, self).__init__()
+
+    def setup(self):
+        # I'm gonna make a mini-canvas...
+        self.canvas = view_plotter.Canvas(
+            height = 600, width = 400, type_str = 'f', fill_value = 0.0
+        )
+        self.scanpath = mock_objects.smi_scanpath_spreadout()
+        self.view_matrix = shapes.Ellipse(0,0,20,20).to_matrix()
+        
+    def teardown(self):
+        pass
+        
+    def test_plotter_draws_on_matrix(self):
+        p = view_plotter.ScanpathPlotter(
+            canvas = self.canvas,
+            scanpath = self.scanpath,
+            view_matrix = self.view_matrix
+        )
+        p.draw_scanpath()
+        p1 = self.scanpath[0]
+        gt_(p.canvas[p1.x][p1.y], 0)
+        
+    
+class TestCanvas(object):
+    def __init__(self):
+        super(TestCanvas, self).__init__()
+    
+    def setup(self):
+        self.rect = shapes.Rectangle(0,0,2,2)
+        self.rmat = self.rect.to_matrix('f', 1.0)
+        self.canvas = view_plotter.Canvas(
+            width = 10, height = 8, 
+            type_str = 'f', fill_value = 0.0
+        )
+        
+    def teardown(self):
+        pass
+    
+    def test_canvas_has_height_and_width(self):
+        c = view_plotter.Canvas(
+            width = 10,
+            height = 8
+        )
+        eq_(c.width(), 10)
+        eq_(c.height(), 8)
+        
+    def test_canvas_is_indexable(self):
+        c = view_plotter.Canvas(
+            width = 10, height = 7, 
+            type_str = 'f', fill_value = 1.0
+        )
+        c[0][0]
+    
+    def test_canvas_sets_proper_type_and_value(self):
+        c = view_plotter.Canvas(
+            width = 10, height = 7, 
+            type_str = 'f', fill_value = 1.0
+        )
+        eq_(c[0][0], 1.0)
+        eq_(type(c[0][0]), type(1.0))
+        
+    def test_canvas_add_matrix_adds_from_origin(self):
+        self.canvas.add_matrix(self.rmat, (0,0))
+        eq_(self.canvas[0][0], 1.0)
+        eq_(self.canvas[1][1], 0.0)
+        
+        
+    def test_canvas_adds_matrix_adds_from_1_1(self):
+        self.canvas.add_matrix(self.rmat, (1,1))
+        eq_(self.canvas[0][0], 1.0)
+        eq_(self.canvas[1][1], 1.0)
+        
+    def test_canvas_adds_matrix_adds_from_oob_topleft(self):
+        self.canvas.add_matrix(self.rmat, (-2,-2))
+        eq_(self.canvas[0][0], 0.0)
+
+    def test_canvas_adds_matrix_adds_from_oob_bottomright(self):
+        self.canvas.add_matrix(self.rmat, (11,9))
+        eq_(self.canvas[9][7], 0.0)
+        
+
+    def test_to_trans_bytes_makes_arrays(self):
+        self.canvas.add_matrix(self.rmat, (11,9))
+        rows = self.canvas.to_trans_bytes()
+        eq_(len(rows), self.canvas.height())
+        for r in rows:
+            eq_(type(r), array.array)
+    
+    def test_file_gets_writ(self):
+        self.canvas.add_matrix(self.rmat, (1,1))
+        self.canvas.to_trans_file('../test.png')
+        
