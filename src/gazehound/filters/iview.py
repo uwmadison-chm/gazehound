4e4a0015e084c3186f7d646597a1bccce8393e91
Nate Vack <njvack@wisc.edu>
2010-02-02 10:02:32 -0600

    Added framework for filtering

---
+++ src/gazehound/filters/iview.py
@@ -0,0 +1,6 @@
+# Part of the gazehound package for analzying eyetracking data
+#
+# Copyright (c) 2010 Board of Regents of the University of Wisconsin System
+#
+# Written by Nathan Vack <njvack@wisc.edu> at the Waisman Laborotory
+# for Brain Imaging and Behavior, University of Wisconsin - Madison.
