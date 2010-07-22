62b42d8f367aaacef08690f135321188f280b526
Nate Vack <njvack@wisc.edu>
2010-07-22 15:15:27 -0500

    Combiner is fast for uniformely sampled scanpaths

--- src/gazehound/viewing.py
+++ src/gazehound/viewing.py
@@ -17,10 +17,33 @@ class Combiner(object):
         self.scanpath = scanpath
 
     def viewings(self):
+        if self.scanpath.uniformely_sampled:
+            return self.__viewings_uniform()
+        else:
+            return self.__viewings_nonuniform()
+    
+    def __viewings_uniform(self):
+        """ Extract viewings (fast) for uniformly-sampled scanpaths """
         t2 = copy.copy(self.timeline)
         for pres in t2:
+            sp = copy.copy(self.scanpath)
+            # Event times are in msec, scanpaths know samples_per_second
+            start_idx = int(sp.samples_per_second*pres.start/1000.0)
+            end_idx = int(sp.samples_per_second*pres.end/1000.0)
+            sp.points = sp.points[start_idx:end_idx]
+            pres.scanpath = sp
+            
+        return timeline.Timeline(t2)
+    
+    def __viewings_nonuniform(self):
+        """ General case: extract viewings for nonuniform paths """
+        t2 = copy.copy(self.timeline)
+        for pres in t2:
+            sp = copy.copy(self.scanpath)
             points = [p for p in self.scanpath
                 if (p.time_midpoint() >= pres.start
                     and p.time_midpoint() < pres.end)]
-            pres.scanpath = gazepoint.Scanpath(points)
+            sp.points = points
+            pres.scanpath = sp
         return timeline.Timeline(t2)
+        
