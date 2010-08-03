846dfc62bfe8572e7621142db940fcee43aa97c5
Nate Vack <njvack@wisc.edu>
2010-08-03 16:03:27 -0500

    Bump odd 2008 copyright date

--- tests/filters/iview_test.py
+++ tests/filters/iview_test.py
@@ -1,7 +1,7 @@
 # coding: utf8
 # Part of the gazehound package for analzying eyetracking data
 #
-# Copyright (c) 2008 Board of Regents of the University of Wisconsin System
+# Copyright (c) 2010 Board of Regents of the University of Wisconsin System
 #
 # Written by Nathan Vack <njvack@wisc.edu> at the Waisman Laborotory
 # for Brain Imaging and Behavior, University of Wisconsin - Madison.
